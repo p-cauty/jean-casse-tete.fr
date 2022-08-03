fcaa071814d91f145cb22470ffa7ff16f07c8abb
PHPeter <phpeter@phpeter.fr>
2022-08-03 14:51:54 +0200

    Switch from Google Analytics to Matomo

--- index.php
+++ index.php
@@ -51,13 +51,20 @@ $cnt = iterator_count($fi);
         <script type="text/javascript">const images_count = <?= $cnt ?>;</script>
         <script src="confetti.browser.min.js"></script>
         <script type="text/javascript" src="script.js"></script>
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-HS9V5YQD5M"></script>
+        <!-- Matomo -->
         <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-HS9V5YQD5M');
+          var _paq = window._paq = window._paq || [];
+          /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+          _paq.push(['trackPageView']);
+          _paq.push(['enableLinkTracking']);
+          (function() {
+            var u="//analytics.phpeter.fr/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '3']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+          })();
         </script>
+        <!-- End Matomo Code -->
     </body>
 </html>
